018986507b46e79733b76cd8c94fbf6105f02a42
Renê <renejr.arraes286@gmail.com>
2020-08-29 11:44:09 -0300

    adding flat buttons

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,26 @@ void main() {
               color: Colors.white,
               fontWeight: FontWeight.bold,
             ),
-          )
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+              FlatButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ],
       )));
 }
