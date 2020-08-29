e3857f08bf5db4ebe6939a002daf9d8cd60c3916
Renê <renejr.arraes286@gmail.com>
2020-08-29 16:29:31 -0300

    adding padding in the components

--- lib/main.dart
+++ lib/main.dart
@@ -3,35 +3,52 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       title: "Counter",
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      home: Stack(
         children: <Widget>[
-          Text(
-            "Peoples: 0",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
+          Image.asset(
+            "images/backgroung.jpg",
+            fit: BoxFit.cover,
+            height: 100.0,
           ),
-          Row(
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlatButton(
-                child: Text(
-                  "+1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
+              Text(
+                "Peoples: 0",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                onPressed: () {},
               ),
-              FlatButton(
-                child: Text(
-                  "-1",
-                  style: TextStyle(fontSize: 40.0, color: Colors.white),
-                ),
-                onPressed: () {},
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  //Início de um botão
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "+1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ), //Término de um botão
+                  //Início de um botão
+                  Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text(
+                        "-1",
+                        style: TextStyle(fontSize: 40.0, color: Colors.white),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ) //Término de um botão
+                ],
               ),
             ],
-          ),
+          )
         ],
       )));
 }
