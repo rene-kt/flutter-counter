aff1acc633cea25d2c91aa5f6346c4efd0e49ec4
Renê <renejr.arraes286@gmail.com>
2020-08-30 10:44:15 -0300

    changing the number using setState and finishing the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,94 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: "Counter",
-      home: Stack(
-        children: <Widget>[
-          Image.asset(
-            "images/backgroung.jpg",
-            fit: BoxFit.cover,
-            height: 100.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "Peoples: 0",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
+  runApp(MaterialApp(title: "Counter", home: Home()));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _people = 0;
+  String _infoText = "You're able to enter";
+
+  void _changePeople(int delta) {
+    setState(() {
+      _people += delta;
+
+      if (_people < 0) {
+        _infoText = "Negative number";
+      } else if (_people <= 10) {
+        _infoText = "You're able to enter";
+      } else if (_people > 10) {
+        _infoText = "The place is already full";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/background.jpg",
+          fit: BoxFit.fill,
+          height: 5000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Peoples: $_people",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  //Início de um botão
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "+1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                //Início de um botão
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
-                  ), //Término de um botão
-                  //Início de um botão
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: FlatButton(
-                      child: Text(
-                        "-1",
-                        style: TextStyle(fontSize: 40.0, color: Colors.white),
-                      ),
-                      onPressed: () {},
+                    onPressed: () {
+                      _changePeople(1);
+                    },
+                  ),
+                ), //Término de um botão
+                //Início de um botão
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(fontSize: 40.0, color: Colors.white),
                     ),
-                  ) //Término de um botão
-                ],
-              ),
-            ],
-          )
-        ],
-      )));
+                    onPressed: () {
+                      _changePeople(-1);
+                    },
+                  ),
+                ) //Término de um botão
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  _infoText,
+                  style: TextStyle(color: Colors.white, fontSize: 10.0),
+                )
+              ],
+            )
+          ],
+        )
+      ],
+    );
+  }
 }
